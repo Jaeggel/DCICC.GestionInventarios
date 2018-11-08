1f5d21c3f1f41dbfa71e5feeba17dd1c5d8f4159
Jaeggel <joel.19088@hotmail.com>
2018-11-08 16:20:50 -0500

    Layout plantilla original y vista del login.

--- DCICC.GestionInventarios/DCICC.GestionInventarios/App_Start/BundleConfig.cs
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/App_Start/BundleConfig.cs
@@ -9,7 +9,31 @@ namespace DCICC.GestionInventarios
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/Tema/vendors/jquery/dist/jquery.min.js",
+                        "~/Content/Tema/vendors/bootstrap/dist/js/bootstrap.min.js",
+                        "~/Content/Tema/vendors/fastclick/lib/fastclick.js",
+                        "~/Content/Tema/vendors/nprogress/nprogress.js",
+                        "~/Content/Tema/vendors/Chart.js/dist/Chart.min.js",
+                        "~/Content/Tema/vendors/gauge.js/dist/gauge.min.js",
+                        "~/Content/Tema/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js",
+                        "~/Content/Tema/vendors/iCheck/icheck.min.js",
+                        "~/Content/Tema/vendors/skycons/skycons.js",
+                        "~/Content/Tema/vendors/Flot/jquery.flot.js",
+                        "~/Content/Tema/vendors/Flot/jquery.flot.pie.js",
+                        "~/Content/Tema/vendors/Flot/jquery.flot.time.js",
+                        "~/Content/Tema/vendors/Flot/jquery.flot.stack.js",
+                        "~/Content/Tema/vendors/Flot/jquery.flot.resize.js",
+                        "~/Content/Tema/vendors/flot.orderbars/js/jquery.flot.orderBars.js",
+                        "~/Content/Tema/vendors/flot-spline/js/jquery.flot.spline.min.js",
+                        "~/Content/Tema/vendors/flot.curvedlines/curvedLines.js",
+                        "~/Content/Tema/vendors/DateJS/build/date.js",
+                        "~/Content/Tema/vendors/jqvmap/dist/jquery.vmap.js",
+                        "~/Content/Tema/vendors/jqvmap/dist/maps/jquery.vmap.world.js",
+                        "~/Content/Tema/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js",
+                        "~/Content/Tema/vendors/moment/min/moment.min.js",
+                        "~/Content/Tema/vendors/bootstrap-daterangepicker/daterangepicker.js",
+                        "~/Content/Tema/build/js/custom.min.js",
+                        "~/Scripts/Site.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -17,14 +41,21 @@ namespace DCICC.GestionInventarios
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                      "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //        "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Tema/vendors/bootstrap/dist/css/bootstrap.min.css",
+                      "~/Content/Tema/vendors/font-awesome/css/font-awesome.min.css",
+                      "~/Content/Tema/vendors/nprogress/nprogress.css",
+                      "~/Content/Tema/vendors/iCheck/skins/flat/green.css",
+                      "~/Content/Tema/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css",
+                      "~/Content/Tema/vendors/jqvmap/dist/jqvmap.min.css",
+                      "~/Content/Tema/vendors/bootstrap-daterangepicker/daterangepicker.css",
+                      "~/Content/Tema/build/css/custom.min.css",
+                      "~/Content/Site.css"));
         }
     }
 }
