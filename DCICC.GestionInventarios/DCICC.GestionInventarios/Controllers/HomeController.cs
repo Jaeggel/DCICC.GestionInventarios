3ba76a0e0c016ccff29aec2392b6b03b136ae8e0
andresUPS <dchisaguano@est.ups.edu.ec>
2018-11-08 12:04:11 -0500

    test1

--- DCICC.GestionInventarios/DCICC.GestionInventarios/Controllers/HomeController.cs
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace DCICC.GestionInventarios.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page..";
+            ViewBag.Message = "Your application description page.hola.";
 
             return View();
         }
