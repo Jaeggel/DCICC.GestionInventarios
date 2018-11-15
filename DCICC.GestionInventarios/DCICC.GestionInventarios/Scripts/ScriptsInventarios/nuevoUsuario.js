dd034c8fc8862b581e2da9c16c0dd90a38cd7b45
andresUPS <dchisaguano@est.ups.edu.ec>
2018-11-15 15:55:49 -0500

    Combobox roles

--- DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/nuevoUsuario.js
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/nuevoUsuario.js
@@ -1,27 +1,36 @@
 ﻿var datosRoles;
-$(document).ready(function () {
 
+function obtenerMetodoRol(url) {
+    console.log(url);
     //Método ajax para traer los roles de la base de datos
     $.ajax({
         dataType: 'json',
-        url: '../iglesia_controler/consultar_Min',
+        url: url,
         type: 'post',
         success: function (data) {
+            console.log(data);
             datosRoles = data;
             console.log("siiiiii: ");
             cargarRolesCmb();
         }
     });
-
-
-});
+}
 
 //Función para cargar los elementos en el combobox de Roles de Usuario
+
+/*$('#mySelect').append($('<option>',
+    {
+        value: i,
+        text: "Option " + i
+    }));*/
+
 function cargarRolesCmb() {
     console.log("tabla: ");
-    var str = '';
+    var str = '<select class="form-control" name="IdRol">';
+    str += '<option value="">Escoga una opción...</option>';
     for (var i = 0; i < datosRoles.length; i++) {
-        str += '<option value="' + datosRoles[i].id_rol+'">' + datosRoles[i].nombre_rol +'</option>';
+        str += '<option value="' + datosRoles[i].IdRol + '">' + datosRoles[i].NombreRol + '</option>';
     };
+    str += '</select>';
     $("#cargarRoles").html(str);
 }
