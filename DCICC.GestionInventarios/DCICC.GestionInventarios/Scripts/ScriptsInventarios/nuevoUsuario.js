3c912f9dc5abda9431493b1151b5b23a6cf5d0cb
andresUPS <dchisaguano@est.ups.edu.ec>
2018-11-15 14:41:17 -0500

    Scripts de combocox

---
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/nuevoUsuario.js
@@ -0,0 +1,27 @@
+﻿var datosRoles;
+$(document).ready(function () {
+
+    //Método ajax para traer los roles de la base de datos
+    $.ajax({
+        dataType: 'json',
+        url: '../iglesia_controler/consultar_Min',
+        type: 'post',
+        success: function (data) {
+            datosRoles = data;
+            console.log("siiiiii: ");
+            cargarRolesCmb();
+        }
+    });
+
+
+});
+
+//Función para cargar los elementos en el combobox de Roles de Usuario
+function cargarRolesCmb() {
+    console.log("tabla: ");
+    var str = '';
+    for (var i = 0; i < datosRoles.length; i++) {
+        str += '<option value="' + datosRoles[i].id_rol+'">' + datosRoles[i].nombre_rol +'</option>';
+    };
+    $("#cargarRoles").html(str);
+}
