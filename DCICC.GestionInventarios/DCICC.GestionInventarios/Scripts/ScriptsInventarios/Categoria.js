eab73f769c3d891fda1f8e60c403326a93ba54c7
andresUPS <dchisaguano@est.ups.edu.ec>
2018-11-16 01:36:35 -0500

    Creación de js para tablas

---
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/Categoria.js
@@ -0,0 +1,43 @@
+﻿var datosCategorias;
+
+function obtenerMetodoRol(url) {
+    console.log(url);
+    //Método ajax para traer los roles de la base de datos
+    $.ajax({
+        dataType: 'json',
+        url: url,
+        type: 'post',
+        success: function (data) {
+            console.log(data);
+            datosCategorias = data;
+            console.log("siiiiii: ");
+            cargarCategoriaTabla();
+        }
+    });
+}
+
+//Función para cargar la tabla de Categorias
+function cargarCategoriaTabla() {
+    var str = '<table class="table  table-bordered">';
+    str += '<thead> <tr> <th>Nombre Categoría</th> <th>Descripción</th> <th>Estado</th> <th>Modificar</th> <th>Deshabilitar</th> </tr> </thead>';
+    str += '<tbody>';
+    for (var i = 0; i < datosCategorias.length; i++) {
+        var nom = "'" + datosCategorias[i].NombreCategoriaActivo + "'";
+        console.log(nom);
+
+        str += '<tr><td>' + datosCategorias[i].NombreCategoriaActivo +
+            '</td><td>' + datosCategorias[i].DescripcionCategoriaActivo;
+
+        if (datosCategorias[i].HabilitadoCategoriaActivoo) {
+            str += '</td><td> Habilitado';
+        } else {
+            str += '</td><td> Deshabilitado';
+        }
+
+        str += '</td><td><button type="button" class="btn btn-info " ><strong><i class="fa fa-pencil-square-o"></i></strong></button>' +
+            '</td><td><button type="button" class="btn btn-danger " ><strong><i class="fa fa-times-circle"></i></strong></button>' +
+            '</td></tr>';
+    };
+    str += '</tbody></table>';
+    $("#tablaModificarCategorias").html(str);
+}
--- DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/nuevoUsuario.js
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/nuevoUsuario.js
@@ -1,11 +1,12 @@
 ﻿var datosRoles;
+var datosUsuarios;
 
-function obtenerMetodoRol(url) {
-    console.log(url);
-    //Método ajax para traer los roles de la base de datos
+function obtenerMetodoRol(url_Rol,url_Usu) {
+    
+    //Método ajax para obtener los roles de la base de datos
     $.ajax({
         dataType: 'json',
-        url: url,
+        url: url_Rol,
         type: 'post',
         success: function (data) {
             console.log(data);
@@ -14,23 +15,70 @@ function obtenerMetodoRol(url) {
             cargarRolesCmb();
         }
     });
+
+    //Método ajax para obtener usuarios de la base de datos
+    $.ajax({
+        dataType: 'json',
+        url: url_Usu,
+        type: 'post',
+        success: function (data) {
+            console.log(data);
+            datosUsuarios = data;
+            console.log("siiiiii: ");
+        }
+    });
+
+
+
 }
 
 //Función para cargar los elementos en el combobox de Roles de Usuario
-
-/*$('#mySelect').append($('<option>',
-    {
-        value: i,
-        text: "Option " + i
-    }));*/
-
 function cargarRolesCmb() {
-    console.log("tabla: ");
-    var str = '<select class="form-control" name="IdRol">';
+    var str = '<select class="form-control" name="IdRol" required>';
     str += '<option value="">Escoga una opción...</option>';
     for (var i = 0; i < datosRoles.length; i++) {
         str += '<option value="' + datosRoles[i].IdRol + '">' + datosRoles[i].NombreRol + '</option>';
     };
     str += '</select>';
     $("#cargarRoles").html(str);
+}
+
+//Función para evitar correos electronicos repertidos
+function comprobarCorreo(correo) {
+    correo = correo.toLowerCase();
+    var comprobar = false;
+    for (var i = 0; i < datosUsuarios.length; i++) {
+        if (datosUsuarios[i].CorreoUsuario == correo) {
+            comprobar = true;
+        }
+    }
+
+    console.log(comprobar);
+
+    if (comprobar == true) {
+        document.getElementById("CorreoUsuario").setCustomValidity("El correo " + correo + " ya existe");
+    } else {
+        document.getElementById("CorreoUsuario").setCustomValidity("");
+    }
+}  
+
+
+//Función para evitar nombres de nick repetidos
+function comprobarNick(nick) {
+
+    nick = nick.toLowerCase();
+    var comprobar = false;
+    for (var i = 0; i < datosUsuarios.length; i++) {
+        if (datosUsuarios[i].NickUsuario == nick) {
+            comprobar = true;
+        }
+    }
+
+    console.log(comprobar);
+    if (comprobar == true) {
+        document.getElementById("NickUsuario").setCustomValidity("El nick de usuario " + nick + " ya existe");
+    } else {
+        document.getElementById("NickUsuario").setCustomValidity("");
+    }
+
 }
