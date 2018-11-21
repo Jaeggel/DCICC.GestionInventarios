be0474b172d67b4176d6a13cf7e470012c6e3863
andresUPS <dchisaguano@est.ups.edu.ec>
2018-11-21 16:20:28 -0500

    Actualizacion tickets

--- DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/Tickets.js
+++ DCICC.GestionInventarios/DCICC.GestionInventarios/Scripts/ScriptsInventarios/Tickets.js
@@ -1,9 +1,14 @@
-﻿var ticketsAbiertos;
+﻿var url_idioma = obtenerIdioma();
+var estados = listaEstadosTicket();
+var url_resuelto;
+var ticketsAbiertos;
 var tiketsEnCurso;
 var ticketsResueltos;
+var idTicketAbierto;
+
 
 function obtenerTicketsAbiertos(url) {
-    url_metodo = url;
+    url_resuelto = url;
     console.log(url);
     //Método ajax para traer las marcas de la base de datos
     $.ajax({
@@ -15,6 +20,7 @@ function obtenerTicketsAbiertos(url) {
             ticketsAbiertos = data;
             contarAbiertos();
             cargarTablaAbiertos();
+            cargarEstadosAbiertoCmb();
             $('#dataTableAbiertos').DataTable({
                 "language": {
                     "url": url_idioma
@@ -34,6 +40,19 @@ function contarAbiertos() {
     $('#numeroAbiertos').html(contAbiertos).show();
 }
 
+function cargarEstadosAbiertoCmb() {
+    var str = '<select id="Estados" class="form-control" name="Estados" required>';
+    str += '<option value="">Escoga una opción...</option>';
+    for (var i = 0; i < estados.length; i++) {
+        if (estados[i] != 'Abierto') {
+            str += '<option value="' + estados[i] + '">' + estados[i] + '</option>';
+        }
+        
+    };
+    str += '</select>';
+    $("#cargarAbiertosCmb").html(str);
+}
+
 //Función para cargar la tabla de Usuarios
 function cargarTablaAbiertos() {
     var str = '<table id="dataTableAbiertos" class="table jambo_table bulk_action  table-bordered" style="width:100%">';
@@ -47,13 +66,68 @@ function cargarTablaAbiertos() {
             '</td><td>' + ticketsAbiertos[i].FechaAperturaTicket +
             '</td><td>' + ticketsAbiertos[i].NombreUsuario;
         str += '</td><td><div class="text-center"><div class="col-md-12 col-sm-12 col-xs-12">' +
-            '<button type="button" class="btn btn-info text-center" data-toggle="modal" data-target="#ModificarTickets" onclick = "formUpdateTipoAct(' + ticketsAbiertos[i].IdTicket + ');"> <strong><i class="fa fa-pencil-square-o"></i></strong></button>' +
+            '<button type="button" class="btn btn-info text-center" data-toggle="modal" data-target="#ModificarTickets" onclick = "formUpdateActivos(' + ticketsAbiertos[i].IdTicket + ');"> <strong><i class="fa fa-pencil-square-o"></i></strong></button>' +
             '</div></div>' +
             '</td></tr>';
     };
     str += '</tbody></table>';
     $("#ticketsAbiertos").html(str);
 }
+
+//Función para setear los valores en los inputs
+function formUpdateActivos(idTicket) {
+    console.log(idTicket);
+    idTicketAbierto = idTicket;
+    for (var i = 0; i < ticketsAbiertos.length; i++) {
+        
+        if (ticketsAbiertos[i].IdTicket == idTicket) {
+            document.getElementById("DescripcionTicket").value = ticketsAbiertos[i].DescripcionTicket ;
+            document.getElementById("PrioridadTicket").value = ticketsAbiertos[i].PrioridadTicket;
+            document.getElementById("FechaAperturaTicket").value = ticketsAbiertos[i].FechaAperturaTicket;
+            document.getElementById("NombreUsuario").value = ticketsAbiertos[i].NombreUsuario;
+        }
+    };
+}
+
+//Función para modificar el Tipo de activo especificado
+function modificarEstadoTicket(url_modificar) {
+    console.log(url_modificar);
+    var cmbEstado = document.getElementById("Estados");
+    var Estado = cmbEstado.options[cmbEstado.selectedIndex].value;
+    var comentario = document.getElementById("ComentarioTicket").value;
+
+    swal({
+        title: 'Confirmación de Actualización',
+        text: "¿Está seguro de modificar el registro?",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#26B99A',
+        cancelButtonColor: '#337ab7',
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.value) {
+            //Método ajax para modificar el usuario de la base de datos
+            $.ajax({
+                data: { "IdTicket": idTicketAbierto, "EstadoTicket": Estado, "ComentarioTicket": comentario },
+                url: url_modificar,
+                type: 'post',
+                success: function () {
+                    $('#ModificarTickets').modal('hide');
+                    showNotify("Actualización exitosa", 'El Ticket se actualizó correctamente', "success");
+                    obtenerTicketsAbiertos(url_resuelto);
+                }, error: function () {
+                    $('#ModificarTickets').modal('hide');
+                    showNotify("Error en la Actualización", 'No se ha podido actualizar el Ticket', "error");
+                }
+            });
+
+        } else {
+            $('#ModificarTickets').modal('hide');
+        }
+    });
+}
+////////////////////////////////////////////TICKET EN CURSO
 
 
 
