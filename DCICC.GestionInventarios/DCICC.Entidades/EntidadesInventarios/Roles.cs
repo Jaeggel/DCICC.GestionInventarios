1bc41b4feaf4d2d4e43765d8d328d24ea29ada76
Jaeggel <joel.19088@hotmail.com>
2018-12-18 17:30:53 -0500

    Corrección actualización nombre rol.

--- DCICC.GestionInventarios/DCICC.Entidades/EntidadesInventarios/Roles.cs
+++ DCICC.GestionInventarios/DCICC.Entidades/EntidadesInventarios/Roles.cs
@@ -6,6 +6,8 @@
 
         public string NombreRol { get; set; }
 
+        public string NombreRolAntiguo { get; set; }
+
         public string DescripcionRol { get; set; }
 
         public bool HabilitadoRol { get; set; }
